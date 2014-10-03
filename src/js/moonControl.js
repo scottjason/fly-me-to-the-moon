2dafeed34390f210f912f65c2c48043fefaa3a25
Scott Jason <scottleejason@gmail.com>
2014-10-03 04:51:44 -0700

    marks spot where spin works

--- src/js/moonControl.js
+++ src/js/moonControl.js
@@ -12,17 +12,18 @@ MoonControl.prototype = {
     this.MoonView.initialize( this.bindListeners.bind( this ) );
   },
   bindListeners : function() {
+    // this.MoonView.spinGlobe();
     document.getElementById( "takeMeHome" ).addEventListener("click", this.takeMeHome.bind( this ), false );
     document.getElementById( "takeControl" ).addEventListener("click", this.takeControl.bind( this ), false );
     document.getElementById( "toTheMoon" ).addEventListener("click", this.sayGoodBye.bind( this ), false );
     document.getElementById( "anywhereOnEarth" ).addEventListener("click", this.anywhereOnEarth.bind( this ), false );
   },
   takeMeHome : function() {
-    this.TakeMeHome.initialize( this.MoonView.scene );
+    // this.TakeMeHome.initialize( this.MoonView.scene );
   },
   takeControl : function() {
-    this.MoonView.renderInstructions();
-    this.TakeOver.initialize( this.MoonView.viewer, this.MoonView.clock, this.MoonView.scene, this.MoonView.canvas );
+    // this.MoonView.renderInstructions();
+    // this.TakeOver.initialize( this.MoonView.viewer, this.MoonView.clock, this.MoonView.scene, this.MoonView.canvas );
   },
   sayGoodBye : function() {
     console.log( "flying to the moon now")
--- src/js/moonView.js
+++ src/js/moonView.js
@@ -1,3 +1,19 @@
+  var options = {
+        animation: false,
+        homeButton: true,
+        sceneModePicker: false,
+        selectionIndicator: false,
+        baseLayerPicker: false,
+        timeline: false,
+        navigationHelpButton: false,
+        navigationInstructionsInitiallyVisible: false
+     }
+  var viewer = new Cesium.Viewer('cesiumContainer', options );
+  var scene = viewer.scene;
+  var clock = viewer.clock;
+  var camera = scene.camera;
+
+
 function MoonView() {
   if (!( this instanceof MoonView )) {
     return new MoonView();
@@ -6,27 +22,29 @@ function MoonView() {
 
 MoonView.prototype = {
   initialize : function( callback ){
-    this.container = 'cesiumContainer';
-    this.options = {
-        animation: false,
-        homeButton: false,
-        sceneModePicker: false,
-        selectionIndicator: false,
-        baseLayerPicker: false,
-        timeline: false,
-        navigationHelpButton: false,
-        navigationInstructionsInitiallyVisible: false
-     }
-    this.viewer = new Cesium.Viewer( this.container, this.options );
-    this.clock = this.viewer.clock;
-    this.scene = this.viewer.scene;
-    this.canvas = this.scene.canvas
+    this.spinGlobe();
     callback();
   },
-  flyToMoon : function(){
+  spinGlobe : function(){
+    function rotate() {
+      Sandcastle.declare( rotate );
+       var vm = viewer.homeButton.viewModel;
+       vm.duration = 0.0;
+       vm.command();
+       vm.duration = 3.0;
+       clock.multiplier = 3 * 60 * 60;
+       scene.preRender.addEventListener( icrf );
+       scene.globe.enableLighting = true;
+    }
+       rotate();
 
-  },
-  readyOrbit : function(){
+  function icrf(scene, time) {
+    if ( scene.mode !== Cesium.SceneMode.SCENE3D ) { return; }
+    var icrfToFixed = Cesium.Transforms.computeIcrfToFixedMatrix( time );
+    if ( Cesium.defined( icrfToFixed )) {
+        scene.camera.transform = Cesium.Matrix4.fromRotationTranslation(icrfToFixed, Cesium.Cartesian3.ZERO);
+      }
+    }
   },
   renderInstructions : function() {
     $( "#giveInstructions" ).html( "'U' moves up | 'D' moves down | 'L' moves left | 'R' moves right | 'B' moves backward | 'F' moves forward" )
