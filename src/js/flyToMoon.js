6d2330649a698e26c0106e94731e284281337468
Scott Jason <scottleejason@gmail.com>
2014-10-03 06:28:37 -0700

    creates handler to stop globe on click event

--- src/js/flyToMoon.js
+++ src/js/flyToMoon.js
@@ -2,74 +2,74 @@
 // var scene = viewer.scene;
 // var clock = viewer.clock;
 
-// // function flyToSanDiego() {
-// //     Sandcastle.declare(flyToSanDiego);
-// //     scene.camera.flyTo({
-// //         destination : Cesium.Cartesian3.fromDegrees(-117.16, 32.71, 15000.0)
-// //     });
-// // }
+// // // function flyToSanDiego() {
+// // //     Sandcastle.declare(flyToSanDiego);
+// // //     scene.camera.flyTo({
+// // //         destination : Cesium.Cartesian3.fromDegrees(-117.16, 32.71, 15000.0)
+// // //     });
+// // // }
 
-// // function flyToLocation() {
-// //     Sandcastle.declare(flyToLocation);
+// // // function flyToLocation() {
+// // //     Sandcastle.declare(flyToLocation);
 
-// //     // Create callback for browser's geolocation
-// //     function fly(position) {
-// //         scene.camera.flyTo({
-// //             destination : Cesium.Cartesian3.fromDegrees(position.coords.longitude, position.coords.latitude, 1000.0)
-// //         });
-// //     }
+// // //     // Create callback for browser's geolocation
+// // //     function fly(position) {
+// // //         scene.camera.flyTo({
+// // //             destination : Cesium.Cartesian3.fromDegrees(position.coords.longitude, position.coords.latitude, 1000.0)
+// // //         });
+// // //     }
 
-// //     // Ask browser for location, and fly there.
-// //     navigator.geolocation.getCurrentPosition(fly);
-// // }
+// // //     // Ask browser for location, and fly there.
+// // //     navigator.geolocation.getCurrentPosition(fly);
+// // // }
 
-// // function viewRectangle() {
-// //     Sandcastle.declare(viewRectangle);
+// // // function viewRectangle() {
+// // //     Sandcastle.declare(viewRectangle);
 
-// //     var west = -77.0;
-// //     var south = 38.0;
-// //     var east = -72.0;
-// //     var north = 42.0;
+// // //     var west = -77.0;
+// // //     var south = 38.0;
+// // //     var east = -72.0;
+// // //     var north = 42.0;
 
-// //     scene.camera.viewRectangle(Cesium.Rectangle.fromDegrees(west, south, east, north));
+// // //     scene.camera.viewRectangle(Cesium.Rectangle.fromDegrees(west, south, east, north));
 
-// //     // Show the rectangle.  Not required; just for show.
-// //     var polylines = scene.primitives.add(new Cesium.PolylineCollection());
-// //     polylines.add({
-// //         positions : Cesium.Cartesian3.fromDegreesArray([
-// //             west, south,
-// //             west, north,
-// //             east, north,
-// //             east, south,
-// //             west, south
-// //         ])
-// //     });
-// // }
+// // //     // Show the rectangle.  Not required; just for show.
+// // //     var polylines = scene.primitives.add(new Cesium.PolylineCollection());
+// // //     polylines.add({
+// // //         positions : Cesium.Cartesian3.fromDegreesArray([
+// // //             west, south,
+// // //             west, north,
+// // //             east, north,
+// // //             east, south,
+// // //             west, south
+// // //         ])
+// // //     });
+// // // }
 
-// // function flyToRectangle() {
-// //     Sandcastle.declare(flyToRectangle);
+// // // function flyToRectangle() {
+// // //     Sandcastle.declare(flyToRectangle);
 
-// //     var west = -90.0;
-// //     var south = 38.0;
-// //     var east = -87.0;
-// //     var north = 40.0;
+// // //     var west = -90.0;
+// // //     var south = 38.0;
+// // //     var east = -87.0;
+// // //     var north = 40.0;
 
-// //     scene.camera.flyToRectangle({
-// //         destination : Cesium.Rectangle.fromDegrees(west, south, east, north)
-// //     });
+// // //     scene.camera.flyToRectangle({
+// // //         destination : Cesium.Rectangle.fromDegrees(west, south, east, north)
+// // //     });
 
-// //     // Show the rectangle.  Not required; just for show.
-// //     var polylines = scene.primitives.add(new Cesium.PolylineCollection());
-// //     polylines.add({
-// //         positions : Cesium.Cartesian3.fromDegreesArray([
-// //             west, south,
-// //             west, north,
-// //             east, north,
-// //             east, south,
-// //             west, south
-// //         ])
-// //     });
-// // }
+// // //     // Show the rectangle.  Not required; just for show.
+// // //     var polylines = scene.primitives.add(new Cesium.PolylineCollection());
+// // //     polylines.add({
+// // //         positions : Cesium.Cartesian3.fromDegreesArray([
+// // //             west, south,
+// // //             west, north,
+// // //             east, north,
+// // //             east, south,
+// // //             west, south
+// // //         ])
+// // //     });
+// // // }
 
 // // function setReferenceFrame() {
 // //     Sandcastle.declare(setReferenceFrame);
@@ -95,85 +95,65 @@
 // //     }));
 // // }
 
-// // function icrf(scene, time) {
-// //     if (scene.mode !== Cesium.SceneMode.SCENE3D) {
-// //         return;
-// //     }
+// // setReferenceFrame()
+// function icrf(scene, time, status) {
+//   // if ( status != 'undefined' )
+//   //   {  return }
+//   // clock.clockRange = Cesium.ClockRange.UNBOUNDED;
+// // clock.clockStep = Cesium.ClockStep.SYSTEM_CLOCK_MULTIPLIER;
+//     if (scene.mode !== Cesium.SceneMode.SCENE3D) {
+//         return;
+//     }
 
-// //     var icrfToFixed = Cesium.Transforms.computeIcrfToFixedMatrix(time);
-// //     if (Cesium.defined(icrfToFixed)) {
-// //         scene.camera.transform = Cesium.Matrix4.fromRotationTranslation(icrfToFixed, Cesium.Cartesian3.ZERO);
-// //     }
+//     var icrfToFixed = Cesium.Transforms.computeIcrfToFixedMatrix(time);
+//     if (Cesium.defined(icrfToFixed)) {
+//         scene.camera.transform = Cesium.Matrix4.fromRotationTranslation(icrfToFixed, Cesium.Cartesian3.ZERO);
+//     }
+// }
+// // icrf();
+// function spin( status ) {
+//     Sandcastle.declare(spin);
+//   if ( status != 'undefined' )
+//     {  return }
+//     var vm = viewer.homeButton.viewModel;
+//     vm.duration = 0.0;
+//     vm.command();
+//     vm.duration = 3.0;
+//     clock.multiplier = 3 * 60 * 60;
+//     scene.preRender.addEventListener(icrf, status );
+//     scene.globe.enableLighting = true;
+// }
+// spin( status )
+// var status = "something"
+// // function createModel(url, height) {
+// //     height = Cesium.defaultValue(height, 0.0);
+
+// //     var modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(-123.0744619, 44.0503706, height));
+
+// //     scene.primitives.removeAll(); // Remove previous model
+// //     var model = scene.primitives.add(Cesium.Model.fromGltf({
+// //         url : url,
+// //         modelMatrix : modelMatrix,
+// //         minimumPixelSize : 128
+// //     }));
+
+// //     model.readyToRender.addEventListener(function(model) {
+// //         // Play and loop all animations at half-spead
+// //         model.activeAnimations.addAll({
+// //             speedup : 0.5,
+// //             loop : Cesium.ModelAnimationLoop.REPEAT
+// //         });
+
+// //         // Zoom to model
+// //         var center = Cesium.Matrix4.multiplyByPoint(model.modelMatrix, model.boundingSphere.center, new Cesium.Cartesian3());
+// //         var transform = Cesium.Transforms.eastNorthUpToFixedFrame(center);
+// //         var camera = scene.camera;
+// //         camera.transform = transform;
+// //         var controller = scene.screenSpaceCameraController;
+// //         var r = 2.0 * Math.max(model.boundingSphere.radius, camera.frustum.near);
+// //         controller.minimumZoomDistance = r * 0.5;
+// //         camera.lookAt(new Cesium.Cartesian3(r, r, r), Cesium.Cartesian3.ZERO, Cesium.Cartesian3.UNIT_Z);
+// //     });
 // // }
 
-// // function spin() {
-// //     Sandcastle.declare(spin);
-
-// //     var vm = viewer.homeButton.viewModel;
-// //     console.log( viewer.homeButton )
-// //     vm.duration = 0.0;
-// //     vm.command();
-// //     vm.duration = 3.0;
-
-// //     clock.multiplier = 3 * 60 * 60;
-// //     scene.preRender.addEventListener(icrf);
-// //     scene.globe.enableLighting = true;
-// // }
-// // spin()
-// // // scene.destroy()
-// // // Sandcastle.reset()
-
-// // Sandcastle.addToolbarMenu([{
-// //     text : 'Camera Options'
-// // }, {
-// //     text : 'Fly to San Diego',
-// //     onselect : function() {
-// //         flyToSanDiego();
-// //         Sandcastle.highlight(flyToSanDiego);
-// //     }
-// // }, {
-// //     text : 'Fly to My Location',
-// //     onselect : function() {
-// //         flyToLocation();
-// //         Sandcastle.highlight(flyToLocation);
-// //     }
-// // }, {
-// //     text : 'Fly to Rectangle',
-// //     onselect : function() {
-// //         flyToRectangle();
-// //         Sandcastle.highlight(flyToRectangle);
-// //     }
-// // }, {
-// //     text : 'View a Rectangle',
-// //     onselect : function() {
-// //         viewRectangle();
-// //         Sandcastle.highlight(viewRectangle);
-// //     }
-// // }, {
-// //     text : 'Set camera reference frame',
-// //     onselect : function() {
-// //         setReferenceFrame();
-// //         Sandcastle.highlight(setReferenceFrame);
-// //     }
-// // }, {
-// //     text : 'View in ICRF',
-// //     onselect : function() {
-// //         viewInICRF();
-// //         Sandcastle.highlight(viewInICRF);
-// //     }
-// // }]);
-
-// // Sandcastle.reset = function() {
-// //     scene.primitives.removeAll();
-// //     scene.tweens.removeAll();
-
-// //     scene.camera.setTransform(Cesium.Matrix4.IDENTITY);
-
-// //     clock.multiplier = 1.0;
-// //     scene.preRender.removeEventListener(icrf);
-// //     scene.globe.enableLighting = false;
-// // };
-
-// // scene.morphComplete.addEventListener(function() {
-// //     Sandcastle.reset();
-// // });
+// // createModel()
--- src/js/moonView.js
+++ src/js/moonView.js
@@ -1,4 +1,13 @@
-  var options = {
+function MoonView() {
+  if (!( this instanceof MoonView )) {
+    return new MoonView();
+  }
+};
+
+MoonView.prototype = {
+  initialize : function( callback ){
+    this.container = 'cesiumContainer';
+    this.options = {
         animation: false,
         homeButton: true,
         sceneModePicker: false,
@@ -8,24 +17,18 @@
         navigationHelpButton: false,
         navigationInstructionsInitiallyVisible: false
      }
-  var viewer = new Cesium.Viewer('cesiumContainer', options );
-  var scene = viewer.scene;
-  var clock = viewer.clock;
-  var camera = scene.camera;
-
-
-function MoonView() {
-  if (!( this instanceof MoonView )) {
-    return new MoonView();
-  }
-};
-
-MoonView.prototype = {
-  initialize : function( callback ){
-    this.spinGlobe();
+    this.viewer = new Cesium.Viewer( this.container, this.options );
+    this.clock = this.viewer.clock;
+    this.scene = this.viewer.scene;
+    this.canvas = this.scene.canvas;
+    // this.spinGlobe()
     callback();
   },
   spinGlobe : function(){
+    var viewer = this.viewer;
+    var scene = this.scene;
+    var clock = this.clock;
+
     function rotate() {
       Sandcastle.declare( rotate );
        var vm = viewer.homeButton.viewModel;
@@ -33,21 +36,24 @@ MoonView.prototype = {
        vm.command();
        vm.duration = 3.0;
        clock.multiplier = 3 * 60 * 60;
-       scene.preRender.addEventListener( icrf );
+       scene.preRender.addEventListener( referenceFrame );
        scene.globe.enableLighting = true;
     }
-       rotate();
-
-  function icrf(scene, time) {
-    if ( scene.mode !== Cesium.SceneMode.SCENE3D ) { return; }
-    var icrfToFixed = Cesium.Transforms.computeIcrfToFixedMatrix( time );
-    if ( Cesium.defined( icrfToFixed )) {
-        scene.camera.transform = Cesium.Matrix4.fromRotationTranslation(icrfToFixed, Cesium.Cartesian3.ZERO);
+      rotate();
+      function referenceFrame( scene, time ) {
+        if ( document.status === null) { clock.multiplier = 1.0 }
+        if ( scene.mode !== Cesium.SceneMode.SCENE3D ) { return; }
+        var icrfToFixed = Cesium.Transforms.computeIcrfToFixedMatrix( time );
+        if ( Cesium.defined( icrfToFixed )) {
+          scene.camera.transform = Cesium.Matrix4.fromRotationTranslation(icrfToFixed, Cesium.Cartesian3.ZERO);
+        }
       }
-    }
   },
   renderInstructions : function() {
     $( "#giveInstructions" ).html( "'U' moves up | 'D' moves down | 'L' moves left | 'R' moves right | 'B' moves backward | 'F' moves forward" )
+  },
+  spinStatus : function( status ){
+    document.status = status;
   }
 }
 
