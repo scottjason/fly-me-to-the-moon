a1b048e5d949fd1afc26ed2ef6d9e279029194e1
Scott Jason <scottleejason@gmail.com>
2014-10-02 23:13:31 -0700

    completes fly me home

--- src/js/takeMeHome.js
+++ src/js/takeMeHome.js
@@ -5,8 +5,28 @@ function TakeMeHome() {
 }
 
 TakeMeHome.prototype = {
-  initialize : function() {
+  initialize : function( viewer, clock, scene ) {
+    this.viewer = viewer;
+    this.clock = clock;
+    this.scene = scene;
+    this.flyHome();
+  },
+  flyHome : function() {
+    var scene = this.scene;
+    function flyToLocation() {
+      Sandcastle.declare(flyToLocation);
+
+    // Create callback for browser's geolocation
+      function fly( position ) {
+        scene.camera.flyTo({
+            destination : Cesium.Cartesian3.fromDegrees(position.coords.longitude, position.coords.latitude, 1000.0)
+        });
+    }
+    // Ask browser for location, and fly there.
+    navigator.geolocation.getCurrentPosition(fly);
   }
+  flyToLocation( scene );
+ }
 }
 
 var takeMeHome = new TakeMeHome();
