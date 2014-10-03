be6b2150857c473a7c3b8b05dfc5c931ac4b5108
Scott Jason <scottleejason@gmail.com>
2014-10-02 22:39:19 -0700

    completes nav restyle

--- src/js/MoonControl.js
+++ src/js/MoonControl.js
@@ -14,6 +14,7 @@ MoonControl.prototype = {
     document.getElementById( "takeControl" ).addEventListener("click", this.takeControl.bind( this ), false );
   },
   takeControl : function() {
+    console.log( this )
     this.UserControl.initialize( this.MoonView.viewer, this.MoonView.clock, this.MoonView.scene, this.MoonView.canvas );
   },
   userSelect : function() {
