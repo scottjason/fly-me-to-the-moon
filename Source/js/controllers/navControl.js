b5f7afd1f39e3048591b93d434078914a0da0939
Scott Jason <scottleejason@gmail.com>
2014-10-05 18:05:51 -0700

    completes moonView refactor

--- Source/js/controllers/navControl.js
+++ Source/js/controllers/navControl.js
@@ -6,47 +6,48 @@ function NavControl() {
 
 function collectHomeObject() {
     var homeButton = document.getElementsByClassName( "cesium-viewer-toolbar" )
-    homeButton[0].addEventListener( "click", NavControl.prototype.slideInAll.call( new NavControl ), false );
+        homeButton[0].addEventListener( "click", NavControl.prototype.slideInAll.bind( new NavControl ), false );
 }
 
 NavControl.prototype = {
-    initialize: function() {
+    initialize : function() {
         this.outOptions = [ "slide", { direction: "right" }, 450 ];
         this.inOptions = [ "slide", { direction: "left" }, 450 ];
         this.bindListeners();
     },
-    bindListeners: function() {
+    bindListeners : function() {
         document.getElementById( "takeMeHome" ).addEventListener( "click", this.homeSlideOut.bind( this ), false );
         document.getElementById( "takeControl" ).addEventListener( "click", this.controlSlideOut.bind( this ), false );
         document.getElementById( "anywhereButHere" ).addEventListener( "click", this.anywhereSlideOut.bind( this ), false );
         document.getElementById( "toTheMoon" ).addEventListener( "click", this.moonSlideOut.bind( this ), false );
         window.setTimeout( collectHomeObject, 2000 );
     },
-    homeSlideOut: function() {
+    homeSlideOut : function() {
         $("#toTheMoon").toggle( this.outOptions[0], this.outOptions[1], this.outOptions[2] );
         $("#anywhereButHere").toggle( this.outOptions[0], this.outOptions[1], this.outOptions[2] );
         $("#takeControl").toggle( this.outOptions[0], this.outOptions[1], this.outOptions[2] );
         $("#takeMeHome").toggle( this.outOptions[0], this.outOptions[1], this.outOptions[2] );
     },
-    controlSlideOut: function() {
+    controlSlideOut : function() {
         $("#toTheMoon").toggle( this.outOptions[0], this.outOptions[1], this.outOptions[2] );
         $("#anywhereButHere").toggle( this.outOptions[0], this.outOptions[1], this.outOptions[2] );
         $("#takeControl").toggle( this.outOptions[0], this.outOptions[1], this.outOptions[2] );
         $("#takeMeHome").toggle( this.outOptions[0], this.outOptions[1], this.outOptions[2] );
     },
-    anywhereSlideOut: function() {
+    anywhereSlideOut : function() {
         $("#toTheMoon").toggle( this.outOptions[0], this.outOptions[1], this.outOptions[2] );
         $("#anywhereButHere").toggle( this.outOptions[0], this.outOptions[1], this.outOptions[2] );
         $("#takeControl").toggle( this.outOptions[0], this.outOptions[1], this.outOptions[2] );
         $("#takeMeHome").toggle( this.outOptions[0], this.outOptions[1], this.outOptions[2] );
     },
-    moonSlideOut: function() {
+    moonSlideOut : function() {
         $( "#toTheMoon" ).toggle( this.outOptions[0], this.outOptions[1], this.outOptions[2] );
         $( "#anywhereButHere" ).toggle( this.outOptions[0], this.outOptions[1], this.outOptions[2] );
         $( "#takeControl" ).toggle( this.outOptions[0], this.outOptions[1], this.outOptions[2] );
         $( "#takeMeHome" ).toggle( this.outOptions[0], this.outOptions[1], this.outOptions[2] );
     },
     slideInAll : function() {
+        console.log( this );
     }
 }
 
--- Source/js/models/globeTerrain.js
+++ Source/js/models/globeTerrain.js
@@ -1,38 +1,38 @@
-// function createGlobe(){
+function createGlobe(){
 
-// Cesium.BingMapsApi.defaultKey = 'AvCHv-7wjmYV1vqauXsrzTQRByL7b8t0F0yG6BhZh-TUjE3-VLvIYxVg4S7OMLMG'
-// var earthOptions = {
-//         animation: false,
-//         homeButton: false,
-//         sceneModePicker: false,
-//         selectionIndicator: false,
-//         baseLayerPicker: false,
-//         timeline: false,
-//         navigationHelpButton: false,
-//         navigationInstructionsInitiallyVisible: false
-// }
-// viewer = new Cesium.Viewer( 'cesiumContainer', earthOptions );
+Cesium.BingMapsApi.defaultKey = 'AvCHv-7wjmYV1vqauXsrzTQRByL7b8t0F0yG6BhZh-TUjE3-VLvIYxVg4S7OMLMG'
+var earthOptions = {
+        animation: false,
+        homeButton: true,
+        sceneModePicker: false,
+        selectionIndicator: false,
+        baseLayerPicker: false,
+        timeline: false,
+        navigationHelpButton: false,
+        navigationInstructionsInitiallyVisible: false
+}
+viewer = new Cesium.Viewer( 'cesiumContainer', earthOptions );
 
-// scene = viewer.scene;
-// globe = scene.globe;
-// clock = viewer.clock;
-// globe.depthTestAgainstTerrain = true;
+scene = viewer.scene;
+globe = scene.globe;
+clock = viewer.clock;
+globe.depthTestAgainstTerrain = true;
 
-// cesiumTerrainProviderHeightmaps = new Cesium.CesiumTerrainProvider({
-//     url : '//cesiumjs.org/smallterrain'
-// });
+cesiumTerrainProviderHeightmaps = new Cesium.CesiumTerrainProvider({
+    url : '//cesiumjs.org/smallterrain'
+});
 
-// cesiumTerrainProviderMeshes = new Cesium.CesiumTerrainProvider({
-//     url : '//cesiumjs.org/stk-terrain/tilesets/world/tiles'
-// });
+cesiumTerrainProviderMeshes = new Cesium.CesiumTerrainProvider({
+    url : '//cesiumjs.org/stk-terrain/tilesets/world/tiles'
+});
 
-// ellipsoidProvider = new Cesium.EllipsoidTerrainProvider();
+ellipsoidProvider = new Cesium.EllipsoidTerrainProvider();
 
-// vrTheWorldProvider = new Cesium.VRTheWorldTerrainProvider({
-//     url : '//www.vr-theworld.com/vr-theworld/tiles1.0.0/73/'
+vrTheWorldProvider = new Cesium.VRTheWorldTerrainProvider({
+    url : '//www.vr-theworld.com/vr-theworld/tiles1.0.0/73/'
 
-// });
+});
 
-// scene.terrainProvider = cesiumTerrainProviderMeshes;
+scene.terrainProvider = cesiumTerrainProviderMeshes;
 
-// }
+}
