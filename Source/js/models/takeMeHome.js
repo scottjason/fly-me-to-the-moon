e9e79e4fd6d818bdb466fb8a80c7f45d17e9da07
Scott Jason <scottleejason@gmail.com>
2014-10-05 19:43:10 -0700

    adds request for location data on user fly home

--- Source/js/models/takeMeHome.js
+++ Source/js/models/takeMeHome.js
@@ -14,9 +14,10 @@ TakeMeHome.prototype = {
       // create callback for user's geolocation
       function fly( position ) {
         TakeMeHome.prototype.createReverseGeo( position, callback );
-         scene.camera.flyTo({
-          destination : Cesium.Cartesian3.fromDegrees( position.coords.longitude, position.coords.latitude, 1000.0 )
-        });
+        TakeMeHome.prototype.collectLocationData( position );
+          scene.camera.flyTo({
+            destination : Cesium.Cartesian3.fromDegrees( position.coords.longitude, position.coords.latitude, 1000.0 )
+          });
        }
     // collect user's geolocation, invoke fly() on complete
     if ( navigator.geolocation && typeof ( navigator.geolocation.getCurrentPosition ) == "function") {
@@ -44,7 +45,19 @@ TakeMeHome.prototype = {
  },
  renderUserAddress : function( location ) {
    this.callback( location );
-  }
+  },
+   collectLocationData: function( position ) {
+        console.log( position )
+        $.ajax({
+            url: 'http://dev.virtualearth.net/REST/v1/Locations/' + position[0] + position[1] + '?o=json&key=AvCHv-7wjmYV1vqauXsrzTQRByL7b8t0F0yG6BhZh-TUjE3-VLvIYxVg4S7OMLMG',
+            type: "GET",
+            dataType: "JSONP",
+            jsonp: "JSONP",
+            success: function( data ) {
+            console.log( data );
+            }
+        })
+    }
 }
 
 var takeMeHome = new TakeMeHome();
