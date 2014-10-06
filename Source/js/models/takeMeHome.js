5c71c89e33142e29e44221e3e0775e16197191cf
Scott Jason <scottleejason@gmail.com>
2014-10-05 23:32:18 -0700

    removes google maps dependecies, creates reverse geo through REST api

--- Source/js/models/takeMeHome.js
+++ Source/js/models/takeMeHome.js
@@ -13,8 +13,7 @@ TakeMeHome.prototype = {
       Sandcastle.declare( flyToLocation );
       // create callback for user's geolocation
       function fly( position ) {
-        TakeMeHome.prototype.createReverseGeo( position, callback );
-        // TakeMeHome.prototype.collectLocationData( position );
+        TakeMeHome.prototype.reverseGeoRequest( position, callback );
           scene.camera.flyTo({
             destination : Cesium.Cartesian3.fromDegrees( position.coords.longitude, position.coords.latitude, 1000.0 )
           });
@@ -30,31 +29,17 @@ TakeMeHome.prototype = {
   // initial call
   flyToLocation( scene );
  },
- createReverseGeo : function( position, callback ) {
-    this.callback = callback;
-    var latLng = new google.maps.LatLng( position.coords.latitude, position.coords.longitude );
-    var coder = new google.maps.Geocoder();
-    coder.geocode( { 'latLng': latLng }, this.formatUserAddress.bind( this ), { maximumAge: 75000 }  );
- },
- formatUserAddress : function( results, status ) {
-   if ( status == google.maps.GeocoderStatus.OK ) {
-     this.callback( results[1].formatted_address )
-  } else {
-    return
+  reverseGeoRequest : function( position, callback ) {
+    $.ajax({
+      url: 'http://dev.virtualearth.net/REST/v1/Locations/' + position.coords.latitude + ',' + position.coords.longitude + '?o=json&key=AvCHv-7wjmYV1vqauXsrzTQRByL7b8t0F0yG6BhZh-TUjE3-VLvIYxVg4S7OMLMG',
+      type: "GET",
+      dataType: "JSONP",
+      jsonp: "JSONP",
+      success: function( data ) {
+        callback ( data.resourceSets[0].resources[0].address.formattedAddress )
+       }
+    })
   }
- },
-   collectLocationData: function( position ) {
-        console.log( position )
-        $.ajax({
-            url: 'http://dev.virtualearth.net/REST/v1/Locations/' + position[0] + position[1] + '?o=json&key=AvCHv-7wjmYV1vqauXsrzTQRByL7b8t0F0yG6BhZh-TUjE3-VLvIYxVg4S7OMLMG',
-            type: "GET",
-            dataType: "JSONP",
-            jsonp: "JSONP",
-            success: function( data ) {
-            console.log( data );
-            }
-        })
-    }
 }
 
 var takeMeHome = new TakeMeHome();
